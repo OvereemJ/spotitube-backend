73126c453be040cfdf56cafee71c6646df6d94a3
OvereemJ <39057313+OvereemJ@users.noreply.github.com>
2019-03-11 11:27:20 +0100

    Add files via upload

---
+++ src/main/java/han/nl/dea/spotitubebackend/App.java
@@ -0,0 +1,13 @@
+package han.nl.dea.spotitubebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
