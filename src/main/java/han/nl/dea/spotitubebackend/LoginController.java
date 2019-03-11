3f980b50a5c6f7039e2ea5ab0c03919b8596f5e9
jorrit <jorrit@solusweb.nl>
2019-03-11 20:00:46 +0100

    Fixed return token wirth post request. Added getters and setters on TokenDTO class

--- src/main/java/han/nl/dea/spotitubebackend/LoginController.java
+++ src/main/java/han/nl/dea/spotitubebackend/LoginController.java
@@ -1,9 +1,6 @@
 package han.nl.dea.spotitubebackend;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -12,7 +9,7 @@ import java.util.Map;
 @Path("login")
 public class LoginController {
 
-    private static Map<String, User> loginMap = new HashMap<>();
+    private static Map<TokenDTO, UserDTO> loginMap = new HashMap<>();
 
     public LoginController (){
 
@@ -21,9 +18,7 @@ public class LoginController {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response LoginUser(User user){
-        loginMap.put(user.generateToken(user.getUsername(), user.getPassword()), user);
-        Response.ok(user).build();
-        return null;
+    public Response loginUser(UserDTO user){
+        return Response.ok(new TokenDTO("123-123-123", "Jorrit Overeem")).build();
     }
 }
---
+++ src/main/java/han/nl/dea/spotitubebackend/TokenDTO.java
@@ -0,0 +1,33 @@
+package han.nl.dea.spotitubebackend;
+
+public class TokenDTO {
+
+    private String token;
+    private String user;
+
+    public TokenDTO() {
+    }
+
+    public TokenDTO(String token, String user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+
+}
