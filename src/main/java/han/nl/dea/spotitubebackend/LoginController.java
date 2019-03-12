73b457b19089f5d1006d0016a24d6c5e11b131d9
jorrit <jorrit@solusweb.nl>
2019-03-12 10:48:57 +0100

    LoginController: Added error handling, return null if user and password are incorrect

--- src/main/java/han/nl/dea/spotitubebackend/LoginController.java
+++ src/main/java/han/nl/dea/spotitubebackend/LoginController.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-@Path("login")
+@Path("/login")
 public class LoginController {
 
     private static Map<TokenDTO, UserDTO> loginMap = new HashMap<>();
@@ -19,6 +19,20 @@ public class LoginController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response loginUser(UserDTO user){
-        return Response.ok(new TokenDTO("123-123-123", "Jorrit Overeem")).build();
+        if(user.getUsername().equals("Jorrit") && user.getPassword().equals("MyPass!23")){
+            //        return Response.status(200)
+//                .header("Access-Control-Allow-Origin","*")
+//                .header("Access-Control-Allow-Credentials","true")
+//                .header("Access-Control-Allow-Headers","origin,content-type,accept,authorization")
+//                .header("Access-Control-Allow-Methods","GET,POST,DELETE,OPTIONS,HEAD")
+//                .entity(new TokenDTO("123-123-123", "Jorrit Overeem"))
+//                .build();
+
+            return Response.ok(new TokenDTO("1234-1234-1234", "Meron Brouwer")).build();
+        } else{
+            return null;
+        }
     }
+
+
 }
